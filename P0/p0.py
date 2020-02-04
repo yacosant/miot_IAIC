0f5b74e5825f49504f8f2d203768e29969876172
Yaco <yacosant@ucm.es>
2020-02-04 19:48:46 +0100

    [P0] Pinta, pero la generación de puntos falla

--- P0/p0.py
+++ P0/p0.py
@@ -19,25 +19,42 @@ import matplotlib.pyplot as plt
 def funcion (x):
     return x*2
 
+def pintaFun(fX,puntos):
+    # Plot the data
+    plt.plot(fX, fX, label='linear')
+    plt.plot(puntos, 'ro')
+    # Add a legend
+    plt.legend()
+
+    # Show the plot
+    plt.show()
 
 def integra_mc(fun, a, b, num_puntos=10000):
     #generar coordenadas X entre a y b
-    puntosX = np.random.randint(a,b,num_puntos)
+    puntosX = np.random.randint(a,b,size=num_puntos)
     print(puntosX)
 
     #crea las Y de f(x)
     puntosFx = fun(puntosX)
 
     #obtiene minimo y maximo de f(x)
-    min=np.amin(puntosFx)
-    max=np.amax(puntosFx)
+    min = np.amin(puntosFx)
+    max = np.amax(puntosFx)
     print("[INFO] Minimo: "+ str(min) + " / Maximo: "+str(max))
 
     #generar Y entre min y max
     puntosY = np.random.randint(min,max,num_puntos)
 
     #devulve que puntos cumplen y<f(x)
+    mask = (puntosY < puntosFx)
+    nPuntosDebajo =  np.sum(mask)
+    print("Numero de puntos por debajo: "+str(nPuntosDebajo)) 
     
+    #combina coordenadas x e i
+    combined = np.vstack((puntosY, puntosX)).T
+
+    #pinta puntos f(x)
+    pintaFun(puntosFx, combined)
 
     return 0
 
@@ -48,6 +65,7 @@ def main ():
     #integra_mc(funcion,0, 30)
     #matriz = np.random.randint(0,2,3,4(1,1))
     integra_mc(funcion, 1, 100, 10000)
+    
     print("End!")
 
 
