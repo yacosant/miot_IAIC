139db5d5538b7a9a42cc72c7e727864b2d130336
Yaco <yacosant@ucm.es>
2020-02-08 18:40:51 +0100

    [P0] Parte vectorial terminada
    Queda la parte iterativa en bucles

--- P0/p0.py
+++ P0/p0.py
@@ -23,18 +23,18 @@ def funcion (x):
     c = 0
     
     #x = np.arange(-10, 10, 0.1)
-    #return ((a * x) ** 2) + (b * x) + c
-    return x*2
+    return ((a * x) ** 2) + (b * x) + c
+    #return x*2
 
 def pintaFun(puntosX, puntosY, x, y, encima, debajo): 
     plt.scatter(x[debajo], y[debajo], marker='+', color = "green")
-    plt.scatter(x[encima], y[encima], marker='+',color = "red")
+    plt.scatter(x[encima], y[encima], marker='+',color = "grey")
     plt.plot(puntosX, puntosY, color = "blue")
     plt.show()
 
 
 def integra_mc(fun, a, b, num_puntos=10000):
-    num_puntos=1000 #para que tarde menos. BORRAR para entrega
+    #num_puntos=1000 #para que tarde menos. BORRAR para entrega
     
     #generar coordenadas X entre a y b
     puntosX = np.arange(a, b, 0.01)
@@ -45,7 +45,7 @@ def integra_mc(fun, a, b, num_puntos=10000):
     #obtiene minimo y maximo de f(x)
     min = 0 
     max = np.amax(puntosY)
-    print("[INFO] Minimo: "+ str(min) + " / Maximo: "+str(max))
+    print("[INFO] Minimo: "+ str(min) + " | Maximo: "+str(max))
     
     x =  a + (b - a)*np.random.random_sample(num_puntos)
     y = np.random.random_sample(num_puntos)*max
@@ -54,72 +54,31 @@ def integra_mc(fun, a, b, num_puntos=10000):
     encima = y >= fun(x)#np.where(y >= fun(x))
     
     #Cuento cuantos puntos quedan por debajo de la funcion
+    #print(debajo)
+    #print(encima)
     nDebajo =  np.sum(debajo)
-    nTotal =  np.sum(x)
-    print("[INFO] Numero de puntos por debajo: "+str(nDebajo)) 
-    print("[INFO] Numero de puntos total: "+str(nTotal)) 
+    nEncima =  np.sum(encima)
+    nTotal = nDebajo+nEncima
+
+    print("[INFO](Numero de puntos)  | Debajo: "+str(nDebajo)+" | Encima: "+str(nEncima)+" | Total: "+str(nEncima+nDebajo)+" |")
+    
+    #print("[INFO] Numero de puntos por debajo: "+str(nDebajo)) 
+    #print("[INFO] Numero de puntos total: "+str(nTotal)) 
 
     area = (nDebajo/nTotal)*(b-a)*max
-    print("[SOLUCION] % Area por debajo: "+str((nDebajo/nTotal)*100)) 
-    print("[SOLUCION] Area por debajo: "+str(area)) 
+    print("[SOLUCION] Area por debajo: "+str((nDebajo/nTotal)*100)+"%") 
+    print("[SOLUCION] Area por debajo: "+str(area)+" unidades") 
     #Pinto la grafica
     pintaFun(puntosX, puntosY, x, y, encima, debajo)
 
-
-    """
-    for size in sizes: 
-        puntosY = np.random.randint(min, max, int(size)) #same
-    """
-
-
-    """
-    tope= int(num_puntos/(b-a))
-    #for i in range(0, tope):
-     #   puntosY = np.random.randint(min,max,99) #????
-    #generar Y entre min y max
-    #puntosY = np.random.randint(min,max,99)
-
-    puntosY = np.random.uniform(min, max, size=(2, num_puntos))
-
-    #devulve que puntos cumplen y<f(x)
-    mask = (puntosY < puntosY)
-
-    # para pintar los superiores distintos sup = (puntosY => puntosY)
-    nPuntosDebajo =  np.sum(mask)
-    print("Numero de puntos por debajo: "+str(nPuntosDebajo)) 
-    
-    #combina coordenadas x e i
-    combined = np.vstack((puntosY, puntosX)).T
-
-    #pinta puntos f(x)
-    pintaFun(puntosX, puntosY, puntosY, combined, mask)
-    """
     return 0
 
 
 def main ():
-    print("Start!")
+    print("[INICIO]")
     np.set_printoptions(threshold=sys.maxsize)
-    #integra_mc(funcion,0, 30)
-    #matriz = np.random.randint(0,2,3,4(1,1))
-    integra_mc(funcion, 1, 100, 10000)
-    
-    print("End!")
+    integra_mc(funcion, 100, 1000, 10000)
+    print("[FINAL]")
 
-
-#main()
-
-
-x =  np.random.random_sample(10)
-y = np.random.random_sample(10)
-
-debajo = y< 2*x #np.where(y < x*2)
-print(x*2)
-print(y)
-print(debajo)
-print(len(x))
-print(len(debajo))
-
-print("----------")
 
 main()
