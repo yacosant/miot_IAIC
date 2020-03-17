4f4109e50fc553366ecf76e8dfd728521206f809
Yaco <yacosant@ucm.es>
2020-03-17 21:55:32 +0100

    P1-parte 2 casi terminada. ya lo dificil está

--- P1/p1.2.py
+++ P1/p1.2.py
@@ -14,137 +14,68 @@ def carga_csv(file_name):
     # suponemos que siempre trabajaremos con float
     return valores.astype(float)
 
-def pinta(puntosX, puntosY):
-    plt.scatter(puntosX, puntosY, marker='+', color = "red")
-
 def coste(X, Y, Theta):
-    """
-    print("teta:"+str(Theta))
-    print("x:"+str(X))
-    print("y:"+str(Y))
-    """
-    
-    """
-    m = len(X)
-    coste = (1 / (2 * m)) * np.dot((np.dot(Theta, X) - Y).T, np.dot(Theta, X) - Y) 
-    print(coste)
-    return coste"""
-    #"""mi codigo:
     H = np.dot(X, Theta)
-    Aux = (H - Y) ** 2
-    return Aux.sum() / (2 * len(X))
-    #"""
+    Aux = (H - Y)
+    dot = np.dot(Aux.T, Aux)
+    return dot / (2 * len(X))
     
 
 def gradiente(X, Y, Theta, alpha):
-    
     NuevaTheta = Theta
     m = np.shape(X)[0]
     n = np.shape(X)[1]
-    H = np.dot(X, Theta)
-    Aux = (H - Y)
-    for i in range(n):#igual a for n in range(columns):
-        Aux_i = Aux * X[:, i]
-        NuevaTheta[i] -= (alpha / m) * Aux_i.sum()
+
+    sumat = [0.] * n
+    for i in range(n):
+        for j in range(m):
+                sumat[i] += (np.dot(X[j],Theta) - Y[j]) * X[j, i]  # Calcula el sumatorio con esta ecuación
+        NuevaTheta[i] -= (alpha / m) * sumat[i]
     return NuevaTheta
     
     
 def descenso_gradiente(X, Y, theta, alpha):
     theta = gradiente(X,Y,theta,alpha)
     costes = coste(X,Y,theta)
-    print("Coste: "+str(costes)+" - theta: "+str(theta))
     return theta,costes
 
 def main():
     iteraciones = 1500
-    alpha = [0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1]
+    alpha = [0.1, 0.3, 0.01, 0.03, 0.001, 0.003]
 
     datos = carga_csv('ex1data2.csv')
-    X = datos[:, 0:-1]#[:, :-1]
+    X = datos[:, 0:-1]
     Y = datos[:, -1:]
 
     Xn, mu, sigma = normalizar(X)
     m = len(X)
     
     fig = plt.figure()
-    ax = fig.gca() #???
+    ax = fig.gca()
 
     costes = []
     thetas = []
     i=0
-    
-    for a in alpha: 
-        # añadimos una columna de 1's a la X
-        Xn = np.hstack([np.ones([m, 1]), Xn])
-        theta = np.array(np.ones((len(Xn[0])))).reshape(len(Xn[0]), 1)
+    # añadimos una columna de 1's a la X
+    Xn = np.hstack([np.ones([m, 1]), Xn])
 
+    for a in alpha: 
+        theta = np.array(np.ones((len(Xn[0])))).reshape(len(Xn[0]), 1)
         fig = plt.figure()
         ax = fig.gca()
         for i in range(iteraciones):
-            #sumatorio = np.array([])
-            print(i)
             theta, coste =  descenso_gradiente(Xn, Y, theta, a)
             ax.plot(i, coste, 'bx')
-            #plt.savefig('coste-'+str(alpha)+'.png')
-            #print(theta)
-        plt.savefig('coste-'+str(a)+'.png')
         thetas.append(theta)
         costes.append(coste)
+        plt.savefig('coste-'+str(a)+'.png')
+        print("alpha: "+str(a)+ " Coste: "+str(coste)+" - theta: "+str(theta))
         fig.clf()
 
-    """
-    plt.plot(X,theta[0] + theta[1]*X,color='blue') #recta definida por las thetas
-    plt.savefig('p1.2-puntosYrecta.png')
-    plt.show()
-    """
-
-    """
-    Theta0, Theta1, Coste = make_data([-10,10], [-1,4], Xn, Y,theta[0],theta[1])   
-    #Contorno
-    plt.figure()
-    plt.contour(Theta0, Theta1, Coste, np.logspace(-2, 3, 20), colors='blue')  
-    plt.plot(theta[0],theta[1], marker='+',color = "red")
-    plt.savefig('p1.2-contorno.png')
-    plt.show()
-
-    # Plot the surface.
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-    surf = ax.plot_surface(Theta0, Theta1, Coste, linewidth=0, antialiased=False,cmap=cm.coolwarm)
-    # Customize the z axis.
-    ax.set_zlim(0,700)
-    ax.zaxis.set_major_locator(LinearLocator(10))
-    ax.zaxis.set_major_formatter(FormatStrFormatter('%.0f'))
-
-    ax.set_xlim(-10,10)
-    ax.xaxis.set_major_locator(LinearLocator(5))
-    ax.xaxis.set_major_formatter(FormatStrFormatter('%.0f'))
-    ax.yaxis.set_major_locator(LinearLocator(5))
-    ax.yaxis.set_major_formatter(FormatStrFormatter('%.0f'))
-
-	# Add a color bar which maps values to colors.
-    fig.colorbar(surf, shrink=0.5, aspect=5)
-
-    plt.savefig('p1.2-3d.png')
-    plt.show()
-    """
-
-
-def make_data(t0_range, t1_range, X, Y,t1,t2):
-    """Genera las matrices X,Y,Z para generar un plot en 3D
-    """
-    step = 0.1
-    Theta0 = np.arange(t0_range[0], t0_range[1], step)
-    Theta1 = np.arange(t1_range[0], t1_range[1], step)
-    Theta0, Theta1 = np.meshgrid(Theta0, Theta1)
-    # Theta0 y Theta1 tienen las misma dimensiones, de forma que
-    # cogiendo un elemento de cada uno se generan las coordenadas x,y
-    # de todos los puntos de la rejilla
-    Coste = np.empty_like(Theta0)
-    for ix, iy in np.ndindex(Theta0.shape):
-        Coste[ix, iy] = coste(X, Y, [Theta0[ix, iy], Theta1[ix, iy]])
-
-    return (Theta0, Theta1, Coste)
+    thetaFinal= thetas[np.argmin(costes)]
+    costeFinal= np.min(costes)
+    aFinal= alpha[np.argmin(costes)]
+    print(thetaFinal,costeFinal,aFinal)
 
 def normalizar(x):
     mu = np.mean(x, axis=0)
