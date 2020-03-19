0b458853544dbd3dbe847704246535b1f23b0804
Yaco <yacosant@ucm.es>
2020-03-19 19:02:48 +0100

    [P2] parte 1 finiquitada

--- P2/p2.py
+++ P2/p2.py
@@ -7,7 +7,6 @@ import scipy.optimize as opt
 
 def carga_csv(file_name):
     valores = read_csv(file_name, header=None).values
-    # suponemos que siempre trabajaremos con float
     return valores.astype(float)
 
 def sigmoid(x):
@@ -17,7 +16,6 @@ def sigmoid(x):
 def pinta_frontera_recta(X, Y, theta):
 
     pinta_puntos(X,Y)
-    #plt.figure()
     x1_min, x1_max = X[:,1].min(), X[:,1].max()
     x2_min, x2_max = X[:, 2].min(), X[:, 2].max()
 
@@ -31,14 +29,11 @@ def pinta_frontera_recta(X, Y, theta):
 
     # el cuarto parámetro es el valor de z cuya frontera se quiere pintar
     plt.contour(xx1, xx2, h, [0.5], linewidths=1, colors='b')
-    #plt.savefig("frontera.pdf")
+    plt.savefig("frontera.png")
     plt.show()
-    #plt.close()
 
 def cost(theta, X, Y):
-    # H = sigmoid(np.matmul(X, np.transpose(theta)))
     H = sigmoid(np.matmul(X, theta))
-    #cost = (- 1 / (len(X))) * np.sum( Y * np.log(H) + (1 - Y) * np.log(1 - H) )
     cost = (- 1 / (len(X))) * (np.dot(Y, np.log(H)) + np.dot((1 - Y), np.log(1 - H)))
     return cost
 
@@ -59,30 +54,15 @@ def pinta_puntos(X,Y):
             mark='+'
             cc='k'
         plt.scatter(X[pos, 1], X[pos,2], marker=mark, c=cc)
-    #plt.show()
 
-def evaluaPorcentaje(X,Y,Theta):    #cambiar funcion!!!
-    """ mi intento de codigo
+def evaluaPorcentaje(X,Y,Theta): 
+    cont = 0
     m = len(X)
-    X = np.hstack([np.ones((m, 1)), X])
-    prediccion = 1 / (1 + np.exp(-np.dot(Theta, X.T)))
-
-    unos = ((prediccion >= 0.5 and Y == 1) or (prediccion < 0.5 and Y == 0))
-    unos.sum()
-    print(str(unos.sum()*100/m)+"% de aciertos")
-    """
-    #codigo git:
-    m = len(X)
-    #X = np.hstack([np.ones((m, 1)), X])
-    z=np.dot(Theta, X.T)
-    pred =1 / (1 + np.exp(-z))
-    count = 0
-
+    prediccion =1 / (1 + np.exp(-np.dot(Theta, X.T)))
     for i in range(m):
-        if (pred.T[i] >= 0.5 and Y[i] == 1) or (pred.T[i] < 0.5 and Y[i] == 0):
-            count += 1
-
-    print('Hay un {}% de aciertos'.format((count/m)*100))
+        if (prediccion.T[i] >= 0.5 and Y[i] == 1) or (prediccion.T[i] < 0.5 and Y[i] == 0):
+            cont += 1
+    print("Hay un "+ str((cont/m)*100) + "% de aciertos")
 
 def main():
     datos = carga_csv('ex2data1.csv')
@@ -90,14 +70,11 @@ def main():
     np.shape(X)         # (97, 1)
     Y = datos[:, -1]
     np.shape(Y)         # (97,)
-    #pinta_puntos(X,Y)
     m = np.shape(X)[0]
     # añadimos una columna de 1's a la X
     X = np.hstack([np.ones([m, 1]), X])
-    #---
-    ## utilizar para invocar a la función de optimización
-    initialTheta = np.zeros(3) 
 
+    initialTheta = np.zeros(3) 
 
     coste = cost(initialTheta,X,Y)
     print("Coste"+ str(coste))
