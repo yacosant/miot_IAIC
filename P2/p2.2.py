17b1d2452d5c5b6eedd845305ddbf6466030646f
Yaco <yacosant@ucm.es>
2020-03-19 22:05:28 +0100

    [P2] Parte 2 casi lista. Falta hacer buble de lamdas

--- P2/p2.2.py
+++ P2/p2.2.py
@@ -16,7 +16,6 @@ def sigmoid(x):
     return s
 
 def pinta_frontera_recta(X, Y, theta):
-
     pinta_puntos(X,Y)
     #plt.figure()
     x1_min, x1_max = X[:,1].min(), X[:,1].max()
@@ -37,16 +36,27 @@ def pinta_frontera_recta(X, Y, theta):
     #plt.close()
 
 def cost(theta, X, Y, lam):
-    # H = sigmoid(np.matmul(X, np.transpose(theta)))
-    H = sigmoid(np.matmul(X, theta))
-    cost = ((- 1 / (len(X))) * (np.dot(Y, np.log(H)) + np.dot((1 - Y), np.log(1 - H)))) + ((lam / (2 * (len(X)))) * np.sum(theta[1:]**2))
-    return cost
+    m = len(X)
+    H = sigmoid(np.dot(theta,X.T))
+    part3= (np.sum(np.power(theta[1:], 2))*lam)/(2*m)
+    part2= (np.log(1-H)).T*(1-Y)
+    part1= (np.log(H)).T*Y
+
+    return -1/m*(np.sum(part1 + part2)) + part3
+
 
 def gradient(theta, XX, Y, lam):
-    H = sigmoid( np.matmul(XX, theta) )
-    grad = (1 / len(Y)) * (np.matmul(XX.T, H - Y)) #= los que sean distintos de cero)
-    grad[1:] +=  ((lam/len(Y)) * theta )
-    return grad
+    H = sigmoid(np.dot(XX, theta))
+    thetaNoZeroReg = np.insert(theta[1:], 0, 0)
+    gradient =  (np.dot(XX.T, (H - Y)) + lam * thetaNoZeroReg) / len(Y) 
+    return np.vstack(gradient)
+
+
+def costeMinimo(XX, Y, lam):
+    initialTheta = np.zeros(len(XX[0])) 
+    result = opt.fmin_tnc(func=cost, x0=initialTheta, fprime=gradient, args=(XX, Y, lam))
+    return result[0]
+
 
 def pinta_puntos(X,Y):
     plt.figure()
@@ -58,43 +68,20 @@ def pinta_puntos(X,Y):
         if i==1:
             mark='+'
             cc='k'
-        plt.scatter(X[pos, 1], X[pos,2], marker=mark, c=cc)
+        plt.scatter(X[pos, 0], X[pos,1], marker=mark, c=cc)
     #plt.show()
 
-def evaluaPorcentaje(X,Y,Theta):    #cambiar funcion!!!
-    """ mi intento de codigo
-    m = len(X)
-    X = np.hstack([np.ones((m, 1)), X])
-    prediccion = 1 / (1 + np.exp(-np.dot(Theta, X.T)))
-
-    unos = ((prediccion >= 0.5 and Y == 1) or (prediccion < 0.5 and Y == 0))
-    unos.sum()
-    print(str(unos.sum()*100/m)+"% de aciertos")
-    """
-    #codigo git:
-    m = len(X)
-    #X = np.hstack([np.ones((m, 1)), X])
-    z=np.dot(Theta, X.T)
-    pred =1 / (1 + np.exp(-z))
-    count = 0
-
-    for i in range(m):
-        if (pred.T[i] >= 0.5 and Y[i] == 1) or (pred.T[i] < 0.5 and Y[i] == 0):
-            count += 1
-
-    print('Hay un {}% de aciertos'.format((count/m)*100))
-
 def plot_decisionboundary(X, Y, theta, poly):
-    plt.clf()
-    plt.figure()
-    x1_min, x1_max = X[:, 0].min(), X[:, 0].max()
-    x2_min, x2_max = X[:, 1].min(), X[:, 1].max()
+    #plt.clf()
+    #plt.figure()
+    x1_min, x1_max = X[:, 1].min(), X[:, 1].max()
+    x2_min, x2_max = X[:, 2].min(), X[:, 2].max()
     xx1, xx2 = np.meshgrid(np.linspace(x1_min, x1_max),np.linspace(x2_min, x2_max))
     h = sigmoid(poly.fit_transform(np.c_[xx1.ravel(), xx2.ravel()]).dot(theta))
     h = h.reshape(xx1.shape)
     plt.contour(xx1, xx2, h, [0.5], linewidths=1, colors='g')
-    plt.savefig("boundary.pdf")
-    plt.close()
+    plt.savefig("boundary.png")
+    #plt.close()
 
 def main():
     datos = carga_csv('ex2data2.csv')
@@ -102,18 +89,23 @@ def main():
     np.shape(X)
     Y = datos[:, -1]
     np.shape(Y)
-    #pinta_puntos(X,Y)
+    pinta_puntos(X,Y)
     m = np.shape(X)[0]
-    # añadimos una columna de 1's a la X
-    X = np.hstack([np.ones([m, 1]), X])
-    #---
-    ## utilizar para invocar a la función de optimización
-    initialTheta = np.zeros(len(X[0]))#np.zeros(3) 
+
+    poly = PolynomialFeatures(6)
+    X2 = poly.fit_transform(X)
+    initialTheta = np.zeros(X2.shape[1]) #np.zeros(len(X[0]))#np.zeros(3) 
     lam = 1
+    
+    coste = cost(initialTheta,X2,Y, lam)
+    print("Coste: "+ str(coste))
+    gradiente  = gradient(initialTheta,X2,Y,lam)
+    print("gradiente: "+ str(gradiente))
 
-    #coste = cost(initialTheta,X,Y, lam)
-    #print("Coste"+ str(coste))
-
+    teta = costeMinimo(X2,Y, lam)
+    plot_decisionboundary(X2, Y, teta, poly)
+    plt.show()
+"""
     #grad = gradient(initialTheta, X, Y)
     #print("Gradiente"+ str(grad))
 
@@ -122,14 +114,14 @@ def main():
     print(result)
     Theta = result[0]
     pinta_puntos(X,Y)
-    plt.show()
+    #plt.show()
 
-    poly = PolynomialFeatures(6)
-    X = poly.fit_transform(X)
-    plot_decisionboundary(X, Y, Theta, poly)
-    poly.fit_transform(X)
+   
+    plot_decisionboundary(X2, Y, result[0], poly)
 
     #pinta_frontera_recta(X,Y,result[0])
     #evaluaPorcentaje(X,Y,initialTheta)
+
+    """
 main()
 
