ba2a77bb0270b8feef090c2699adc3bc100e5a5a
Yaco <yacosant@ucm.es>
2020-03-30 20:46:23 +0200

    [P3] Fin parte 1

--- P3/p3.py
+++ P3/p3.py
@@ -1,6 +1,5 @@
 import numpy as np
 import copy
-#from pandas.io.parsers import read_csv
 from scipy.io import loadmat
 import matplotlib.pyplot as plt
 import scipy.optimize as opt
@@ -21,32 +20,30 @@ def gradient(theta, XX, Y):
     return grad
 
 def cost_regul(theta, X, Y, lam):
-    m = len(X)
-    H = sigmoid(np.dot(theta,X.T))
-    part3= (np.sum(np.power(theta[1:], 2))*lam)/(2*m)
-    part2= (np.log(1-H)).T*(1-Y)
-    part1= (np.log(H)).T*Y
-
-    return -1/m*(np.sum(part1 + part2)) + part3
+	m = len(X)
+	coste1 = -(1 / m) * np.sum(Y.T * np.log(sigmoid(np.matmul(X, theta))) + (1 - Y.T) * np.log(1 - sigmoid(np.matmul(X, theta))))
+	coste2 = (lam / (2 * m)) * np.sum(theta[1:]**2)
+	return coste1 + coste2
 
 def gradient_regul(theta, XX, Y, lam):
-    H = sigmoid(np.dot(XX, theta))
+    H = np.array([sigmoid(np.dot(XX, theta))]).T
     thetaNoZeroReg = np.insert(theta[1:], 0, 0)
+    thetaNoZeroReg =np.array([np.hstack([0, theta[1:]])]).T
     gradient =  (np.dot(XX.T, (H - Y)) + lam * thetaNoZeroReg) / len(Y) 
-    return np.vstack(gradient)
+    return gradient
+
 
 def costeMinimo(XX, Y, lam):
     initialTheta = np.zeros(len(XX[0])) 
-    result = opt.fmin_tnc(func=cost, x0=initialTheta, fprime=gradient, args=(XX, Y, lam))
+    result = opt.fmin_tnc(func=cost_regul, x0=initialTheta, fprime=gradient_regul, args=(XX, Y, lam)) #regularizado
     return result[0]
 
 def oneVsAll(X, y, num_etiquetas, reg):
     thetas = np.zeros((num_etiquetas, len(X[0])))
     y[y == 10] = 0
     for e in range(num_etiquetas):
-        if y == e:  yy = 1
-        else: yy = 0
-        thetas[e] = costeMinimo(X, yy, reg)  # Calcula el coste de que sea esa etiqueta
+        yy = (y == e).astype('int')
+        thetas[e] = costeMinimo(X, yy, reg)
     return thetas
 
 def clasificador(X, Thetas):
@@ -63,16 +60,16 @@ def exec(X, thetas,n):
         plt.imshow(X[sample, :].reshape(-1, 20).T)
         plt.axis('off')
         val = clasificador(X[sample, :], thetas)  
-        plt.title('Se clasifica como: {}'.format(val))
-        plt.savefig('ejemplo{}.png'.format(i)) 
+        plt.title("Se clasifica como: "+str(val))
+        plt.savefig("ejemplo"+str(i)+".png")
         plt.show()
 
 def evaluaPorcentaje(X,Y,Theta): 
     cont = 0
     m = len(X)
-    prediccion =1 / (1 + np.exp(-np.dot(Theta, X.T)))
     for i in range(m):
-        if (prediccion.T[i] >= 0.5 and Y[i] == 1) or (prediccion.T[i] < 0.5 and Y[i] == 0):
+        clasificado = clasificador(X[i], Theta)
+        if (clasificado == Y[i]):
             cont += 1
     print("Hay un "+ str((cont/m)*100) + "% de aciertos")
 
@@ -86,12 +83,9 @@ def main():
     plt.axis('off')
     plt.show()
 
-
     thetas = oneVsAll(X, y, 10, 0.1)
     exec(X, thetas,10)
     evaluaPorcentaje(X,y,thetas)
 
-    #repasar evalua evaluaPorcentaje
-    #y onVsAll falla por comparar array con nuemeor. invesgar
 #######
 main()
