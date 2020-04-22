8a4062c9cd14d37565a85bd5be35a37fccd03c73
Yaco <yacosant@ucm.es>
2020-04-22 21:53:50 +0200

    [P6] Ejercicio 4 funcional, queda limar

--- P6/p6.py
+++ P6/p6.py
@@ -7,24 +7,37 @@ from keras.layers import Dense
 from sklearn.metrics import classification_report
 from keras.utils.np_utils import to_categorical
 
-def problemaX(num, model, X, y, ver, ep):
-    help.plot_data(X,y)
-    help.plt.savefig("problema"+str(num)+"-"+str(ver)+"-graf0.png")
+def problemaX(num, model, X, y, ver, ep, multi=False, yy=0):
+    if multi==False:
+        help.plot_data(X,y)
+        help.plt.savefig("problema"+str(num)+"-"+str(ver)+"-graf0.png")
 
     history = model.fit(x=X, y=y, verbose=0, epochs=ep)
     help.plot_loss_accuracy(history)
-    help.plt.close(2)
+    if multi==False:
+        help.plt.close(2)
+    else:
+        help.plt.close(1)
+    
     help.plt.savefig("problema"+str(num)+"-"+str(ver)+"-graf1.png")
 
-    #meter un IF segun si es multiclass
-    help.plot_decision_boundary(lambda x: model.predict(x), X, y)
+    #IF segun si es multiclass
+    if multi==False:
+        help.plot_decision_boundary(lambda x: model.predict(x), X, y)
+    else:
+        help.plot_multiclass_decision_boundary(model, X, yy)
+    
     help.plt.savefig("problema"+str(num)+"-"+str(ver)+"-graf2.png")
 
-    help.plot_confusion_matrix(model, X, y)
+    help.plot_confusion_matrix(model, X, yy)
     help.plt.savefig("problema"+str(num)+"-"+str(ver)+"-graf3.png")
-    
-    pred= help.np.concatenate( (model.predict(X)>0.5), axis=0 ).astype(int)
-    print(classification_report(y, pred, target_names=['0', '1']))
+
+    if multi==False:
+        pred= help.np.concatenate( (model.predict(X)>0.5), axis=0 ).astype(int)
+    else:
+        pred= (model.predict(X)>0.5).astype(int)
+
+    print(classification_report(y, pred))
     help.plt.show()
 
 def main():
@@ -85,11 +98,16 @@ def main():
     model40 = Sequential()
     model40.add(Dense(units=4, input_shape=(2,), activation='tanh'))
     model40.add(Dense(units=2, activation='tanh'))
-    model40.add(Dense(units=1, activation='sigmoid'))
+    model40.add(Dense(units=3, activation='softmax'))
     
     model40.compile(Adam(lr=0.01), loss='categorical_crossentropy', metrics=['acc'])
-    problemaX(4,model40, X4,y4,0,100)
+    y4_cat = to_categorical(y4)
+    problemaX(4,model40, X4,y4_cat,0,100, True,y4)
 
+    """
+    X_train , X_test , y_train , y_test = \
+        t r a i n _ t e s t _ s p l i t (X, y , t e s t _ s i z e =0.33 , s t r a t i f y=y )
+"""
 
 
 main()
