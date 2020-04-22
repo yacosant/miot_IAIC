cfacb6feaeee0a6bc7b1411c764031980239a137
Yaco <yacosant@ucm.es>
2020-04-22 20:22:50 +0200

    [P6] fixes quitando variables y comentarios

--- P6/p6.py
+++ P6/p6.py
@@ -20,13 +20,10 @@ def problemaX(num, model, X, y, ver, ep):
 
     help.plot_confusion_matrix(model, X, y)
     help.plt.savefig("problema"+str(num)+"-"+str(ver)+"-graf3.png")
-    #help.plt.show()
-    #print(classification_report(y, model.predict(X), target_names=['0', '1']))
+    
     pred= help.np.concatenate( (model.predict(X)>0.5), axis=0 ).astype(int)
-    #pred = (pred>0.5)#.astype('int')#.ravel()
-    #return  #model.predict(X)
     print(classification_report(y, pred, target_names=['0', '1']))
-
+    help.plt.show()
 
 def main():
 
@@ -37,7 +34,7 @@ def main():
     model10 = Sequential()
     model10.add(Dense(units=1, input_shape=(2,), activation='sigmoid'))
     model10.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
-    y_p10 = problemaX(1,model10, X1,y1,0,50)
+    problemaX(1,model10, X1,y1,0,50)
     
     #2 - version 0 -Regresión logistica no lineal - lunas
     X2, y2 = make_moons(n_samples=1000, noise=0.05, random_state=0)
@@ -46,7 +43,7 @@ def main():
     model20.add(Dense(units=1, input_shape=(2,), activation='sigmoid'))
     
     model20.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
-    y_p20 = problemaX(2,model20, X2,y2,0,100)
+    problemaX(2,model20, X2,y2,0,100)
     
     #2 - version 1 - Cambiando el modelo para adaptarlo a las lunas
     
@@ -57,7 +54,7 @@ def main():
 
     #model21.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
     model21.compile(Adam(lr=0.01), loss='binary_crossentropy', metrics=['acc'])
-    y_p21 = problemaX(2,model21, X2,y2,1,100)
+    problemaX(2,model21, X2,y2,1,100)
     
     #3 - version 0 -Regresión logistica no lineal - circulos
     X3, y3 = make_circles(n_samples=1000, noise=0.05, factor=0.3, random_state=0)
@@ -76,12 +73,11 @@ def main():
     model31.add(Dense(units=2, activation='tanh'))
     model31.add(Dense(units=1, activation='sigmoid'))
     
-    ###model31.compile(Adam(lr=0.01), loss='binary_crossentropy', metrics=['acc'])
-    model31.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+    model31.compile(Adam(lr=0.01), loss='binary_crossentropy', metrics=['acc'])
     problemaX(3,model31, X3,y3,0,100)
-    #print(classification_report(y3, y_p31, target_names=['0', '1']))
-    #print("Y_true:" + str(y3))
-    #print("Y_pred:" + str(y_p31))
+
+
+
 main()
 #comentar que en helper -> cambiado  plt.cm.RdYlBu  y todas las referencias similares
     # por -> plt.cm.get_cmap("RdYlBu")
