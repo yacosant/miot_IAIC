fd559e14ef342ad5efb222c0c8f5ffbc0bd4d26b
Yaco <yacosant@ucm.es>
2020-04-26 14:42:52 +0200

    [P6] Con menú

--- P6/p6.py
+++ P6/p6.py
@@ -40,74 +40,105 @@ def problemaX(num, model, X, y, ver, ep, multi=False, yy=0):
     print(classification_report(y, pred))
     help.plt.show()
 
+def menu():
+    print("---MENU---\n")
+    print("Ejercicios:\n")
+    print("(1) - [2 Dimensiones] Regresión logistica de dos dimensiones")
+    print("(2) - [Lunas v1] ")
+    print("(3) - [Lunas v2] - Clasifica bien")
+    print("(4) - [Circulos v1] ")
+    print("(5) - [Circulos v2] - Clasifica bien")
+    print("(6) - [Espiral v1] ")
+    print("(7) - [Espiral v2] - Clasifica bien")
+    print("(0) - Salir")
+
+    return int(input("Seleccione ejercicio a calcular (numero):"))
+
+
 def main():
+    op = menu()
+    if op==0:
+        print("Saliendo...")
+    elif op==1:
+        #1 - Regresión logistica de dos dimensiones (con keras)
+        
+        X1, y1 = make_classification(n_samples=1000, n_features=2,
+                                n_redundant=0, n_informative=2, random_state=7, n_clusters_per_class=1)
+        model10 = Sequential()
+        model10.add(Dense(units=1, input_shape=(2,), activation='sigmoid'))
+        model10.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+        problemaX(1,model10, X1,y1,0,50)
+    
+    elif  op==2:
+        #2 - version 0 -Regresión logistica no lineal - lunas
+        X2, y2 = make_moons(n_samples=1000, noise=0.05, random_state=0)
+        
+        model20 = Sequential()
+        model20.add(Dense(units=1, input_shape=(2,), activation='sigmoid'))
+        
+        model20.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+        problemaX(2,model20, X2,y2,0,100)
+    
+    elif  op==3:
+        #2 - version 1 - Cambiando el modelo para adaptarlo a las lunas
+        
+        model21 = Sequential()
+        model21.add(Dense(units=4, input_shape=(2,), activation='tanh'))
+        model21.add(Dense(units=2, activation='tanh'))
+        model21.add(Dense(units=1, activation='sigmoid'))
 
-    #1 - Regresión logistica de dos dimensiones (con keras)
-    """
-    X1, y1 = make_classification(n_samples=1000, n_features=2,
-                            n_redundant=0, n_informative=2, random_state=7, n_clusters_per_class=1)
-    model10 = Sequential()
-    model10.add(Dense(units=1, input_shape=(2,), activation='sigmoid'))
-    model10.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
-    problemaX(1,model10, X1,y1,0,50)
-    
-    #2 - version 0 -Regresión logistica no lineal - lunas
-    X2, y2 = make_moons(n_samples=1000, noise=0.05, random_state=0)
-    
-    model20 = Sequential()
-    model20.add(Dense(units=1, input_shape=(2,), activation='sigmoid'))
-    
-    model20.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
-    problemaX(2,model20, X2,y2,0,100)
-    
-    #2 - version 1 - Cambiando el modelo para adaptarlo a las lunas
-    
-    model21 = Sequential()
-    model21.add(Dense(units=4, input_shape=(2,), activation='tanh'))
-    model21.add(Dense(units=2, activation='tanh'))
-    model21.add(Dense(units=1, activation='sigmoid'))
+        #model21.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+        model21.compile(Adam(lr=0.01), loss='binary_crossentropy', metrics=['acc'])
+        problemaX(2,model21, X2,y2,1,100)
 
-    #model21.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
-    model21.compile(Adam(lr=0.01), loss='binary_crossentropy', metrics=['acc'])
-    problemaX(2,model21, X2,y2,1,100)
-    
-    #3 - version 0 -Regresión logistica no lineal - circulos
-    X3, y3 = make_circles(n_samples=1000, noise=0.05, factor=0.3, random_state=0)
-    
-    model30 = Sequential()
-    model30.add(Dense(units=1, input_shape=(2,), activation='sigmoid'))
-    
-    model30.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
-    y_p30 = problemaX(3,model30, X3,y3,0,100)
-    
-    #3 - version 1 -Regresión logistica no lineal - circulos
-    X3, y3 = make_circles(n_samples=1000, noise=0.05, factor=0.3, random_state=0)
-    
-    model31 = Sequential()
-    model31.add(Dense(units=4, input_shape=(2,), activation='tanh'))
-    model31.add(Dense(units=2, activation='tanh'))
-    model31.add(Dense(units=1, activation='sigmoid'))
-    
-    model31.compile(Adam(lr=0.01), loss='binary_crossentropy', metrics=['acc'])
-    problemaX(3,model31, X3,y3,1,100)
-    """
-    
-    #4 - version 0 -Regresión logistica no lineal - Espiral
-    X4, y4 = help.make_multiclass(K=3)
-    
-    model40 = Sequential()
-    model40.add(Dense(units=4, input_shape=(2,), activation='tanh'))
-    model40.add(Dense(units=2, activation='tanh'))
-    model40.add(Dense(units=3, activation='softmax'))
-    
-    model40.compile(Adam(lr=0.01), loss='categorical_crossentropy', metrics=['acc'])
-    y4_cat = to_categorical(y4)
-    problemaX(4,model40, X4,y4_cat,0,100, True,y4)
+    elif  op==4:  
+        #3 - version 0 -Regresión logistica no lineal - circulos
+        X3, y3 = make_circles(n_samples=1000, noise=0.05, factor=0.3, random_state=0)
+        
+        model30 = Sequential()
+        model30.add(Dense(units=1, input_shape=(2,), activation='sigmoid'))
 
-    """
-    X_train , X_test , y_train , y_test = \
-        t r a i n _ t e s t _ s p l i t (X, y , t e s t _ s i z e =0.33 , s t r a t i f y=y )
-"""
+        model30.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+        problemaX(3,model30, X3,y3,0,100)
+    elif  op==5:        
+        #3 - version 1 -Regresión logistica no lineal - circulos
+        X3, y3 = make_circles(n_samples=1000, noise=0.05, factor=0.3, random_state=0)
+        
+        model31 = Sequential()
+        model31.add(Dense(units=4, input_shape=(2,), activation='tanh'))
+        model31.add(Dense(units=2, activation='tanh'))
+        model31.add(Dense(units=1, activation='sigmoid'))
+        
+        model31.compile(Adam(lr=0.01), loss='binary_crossentropy', metrics=['acc'])
+        problemaX(3,model31, X3,y3,1,100)
+        
+    elif  op==6:       
+        #4 - version 0 -Regresión logistica no lineal - Espiral
+        X4, y4 = help.make_multiclass(K=3)
+        
+        model40 = Sequential()
+        model40.add(Dense(units=4, input_shape=(2,), activation='tanh'))
+        model40.add(Dense(units=2, activation='tanh'))
+        model40.add(Dense(units=3, activation='softmax'))
+        
+        model40.compile(Adam(lr=0.01), loss='categorical_crossentropy', metrics=['acc'])
+        y4_cat = to_categorical(y4)
+        problemaX(4,model40, X4,y4_cat,0,100, True,y4)
+
+    elif  op==7:        
+        #4 - version 1 -Regresión logistica no lineal - Espiral mejorada
+        
+        model41 = Sequential()
+        model41.add(Dense(units=4, input_shape=(2,), activation='tanh'))
+        model41.add(Dense(units=2, activation='tanh'))
+        model41.add(Dense(units=3, activation='softmax'))
+        
+        model41.compile(Adam(lr=0.01), loss='categorical_crossentropy', metrics=['acc'])
+        y4_cat = to_categorical(y4)
+        problemaX(4,model41, X4,y4_cat,0,100, True,y4)
+
+    else:
+        print("Opcion erronea")
 
 
 main()
